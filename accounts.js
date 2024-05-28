6049e12505a456340a58a8bd32584072cf83a16d
stdavis <stdavis@utah.gov>
2024-05-28 17:39:58 -0600

    feat!: add support for experience builder
    BREAKING CHANGE:
    
    This project is now a true proxy that should work with any route that answers on the destination server. This dramatically simplifies account data as well as usage.
    
    See [the new docs](README.md#Usage) for the new usage.
    
    Fixes #124,  closes #53

--- accounts.js
+++ accounts.js
@@ -1,135 +1,101 @@
 export default {
   // used by tests
+  '-2': {
+    arcgisServer: 'http://127.0.0.1:8085',
+    quadWord: 'verify-quad-word',
+  },
   '-1': {
-    serviceUrl: 'https://utility.arcgisonline.com/arcgis/rest/services/Utilities/PrintingTools/GPServer',
-    exportTaskName: 'Export Web Map Task',
-    getTemplatesTaskName: 'Get Layout Templates Info Task',
+    arcgisServer: 'https://utility.arcgisonline.com',
     quadWord: 'test-quad-word',
   },
   1: {
     // Created for Tom Thompson at DOGM
-    serviceUrl: 'https://maps.dnr.utah.gov/arcgis/rest/services/Utilities/PrintingTools/GPServer',
-    exportTaskName: 'Export Web Map Task',
-    getTemplatesTaskName: 'Get Layout Templates Info Task',
+    arcgisServer: 'https://maps.dnr.utah.gov',
     quadWord: 'famous-florida-quiet-passive',
   },
   2: {
     // agrc agol org
-    serviceUrl: 'https://utility.arcgisonline.com/arcgis/rest/services/Utilities/PrintingTools/GPServer',
-    exportTaskName: 'Export Web Map Task',
-    getTemplatesTaskName: 'Get Layout Templates Info Task',
+    arcgisServer: 'https://utility.arcgisonline.com',
     quadWord: 'temple-memphis-medusa-corner',
   },
   3: {
     // salt lake county
-    serviceUrl: 'https://slco.org/slcogis/rest/services/Utilities/PrintingTools/GPServer',
-    exportTaskName: 'Export Web Map Task',
-    getTemplatesTaskName: 'Get Layout Templates Info Task',
+    arcgisServer: 'https://slco.org',
     quadWord: 'optimal-polygon-contour-local',
   },
   5: {
     // parcels.utah.gov
-    serviceUrl: 'https://mapserv.utah.gov/arcgis/rest/services/Utilities/PrintingTools/GPServer',
-    exportTaskName: 'Export Web Map Task',
-    getTemplatesTaskName: 'Get Layout Templates Info Task',
+    arcgisServer: 'https://mapserv.utah.gov',
     quadWord: 'alfred-plaster-crystal-dexter',
   },
   6: {
     // wfrc
-    serviceUrl: 'https://utility.arcgisonline.com/arcgis/rest/services/Utilities/PrintingTools/GPServer',
-    exportTaskName: 'Export Web Map Task',
-    getTemplatesTaskName: 'Get Layout Templates Info Task',
+    arcgisServer: 'https://utility.arcgisonline.com',
     quadWord: 'chance-source-liquid-nelson',
   },
   7: {
     // atlas.utah.gov
-    serviceUrl: 'https://mapserv.utah.gov/arcgis/rest/services/Utilities/PrintingTools/GPServer',
-    exportTaskName: 'Export Web Map Task',
-    getTemplatesTaskName: 'Get Layout Templates Info Task',
+    arcgisServer: 'https://mapserv.utah.gov',
     quadWord: 'career-exhibit-panel-stadium',
   },
   9: {
     // wri - dev
-    serviceUrl: 'https://wrimaps.at.utah.gov/arcgis/rest/services/WRI_DEV/Print/GPServer',
-    exportTaskName: 'Export Web Map',
-    getTemplatesTaskName: '',
+    arcgisServer: 'https://wrimaps.at.utah.gov',
     quadWord: 'fractal-crack-beach-cello',
   },
   10: {
     // wri - at
-    serviceUrl: 'https://wrimaps.at.utah.gov/arcgis/rest/services/WRI_AT/Print/GPServer',
-    exportTaskName: 'Export Web Map',
-    getTemplatesTaskName: '',
+    arcgisServer: 'https://wrimaps.at.utah.gov',
     quadWord: 'fractal-crack-beach-cello',
   },
   11: {
     // wri - prod
-    serviceUrl: 'https://wrimaps.utah.gov/arcgis/rest/services/WRI/Print/GPServer',
-    exportTaskName: 'Export Web Map',
-    getTemplatesTaskName: '',
+    arcgisServer: 'https://wrimaps.utah.gov',
     quadWord: 'delete-prefix-stretch-giant',
   },
   12: {
     // summit county
-    serviceUrl: 'https://maps.summitcounty.org/arcgis/rest/services/Geoprocessing/ExportWebMap/GPServer',
-    exportTaskName: 'Export Web Map',
-    getTemplatesTaskName: 'Get Layout Templates Info',
+    arcgisServer: 'https://maps.summitcounty.org',
     quadWord: 'spark-sardine-domain-telecom',
   },
   13: {
     // south davis sewer district
-    serviceUrl: 'https://utility.arcgisonline.com/arcgis/rest/services/Utilities/PrintingTools/GPServer',
-    exportTaskName: 'Export Web Map Task',
-    getTemplatesTaskName: 'Get Layout Templates Info Task',
+    arcgisServer: 'https://utility.arcgisonline.com',
     quadWord: 'congo-fuji-philips-bucket',
   },
   14: {
     // enviro.deq.utah.gov
-    serviceUrl: 'https://utility.arcgisonline.com/arcgis/rest/services/Utilities/PrintingTools/GPServer',
-    exportTaskName: 'Export Web Map Task',
-    getTemplatesTaskName: 'Get Layout Templates Info Task',
+    arcgisServer: 'https://utility.arcgisonline.com',
     quadWord: 'result-table-secure-antenna',
   },
   15: {
     // broadband.utah.gov/map
-    serviceUrl: 'https://mapserv.utah.gov/arcgis/rest/services/Broadband/ExportWebMap/GPServer',
-    exportTaskName: 'Export Web Map',
-    getTemplatesTaskName: '',
+    arcgisServer: 'https://mapserv.utah.gov',
     quadWord: 'reserve-tactic-cricket-spider',
   },
   16: {
     // dnr agol
-    serviceUrl: 'https://utility.arcgisonline.com/arcgis/rest/services/Utilities/PrintingTools/GPServer',
-    exportTaskName: 'Export Web Map Task',
-    getTemplatesTaskName: 'Get Layout Templates Info Task',
+    arcgisServer: 'https://utility.arcgisonline.com',
     quadWord: 'famous-florida-quiet-passive',
   },
   17: {
     // salt lake county - with ESRI print service
-    serviceUrl: 'https://utility.arcgisonline.com/arcgis/rest/services/Utilities/PrintingTools/GPServer',
-    exportTaskName: 'Export Web Map Task',
-    getTemplatesTaskName: 'Get Layout Templates Info Task',
+    arcgisServer: 'https://utility.arcgisonline.com',
     quadWord: 'optimal-polygon-contour-local',
   },
   18: {
     // carbon county
-    serviceUrl: 'https://maps.carbon.utah.gov/arcgis/rest/services/Utilities/PrintingTools/GPServer',
-    exportTaskName: 'Export Web Map Task',
-    getTemplatesTaskName: 'Get Layout Templates Info Task',
+    arcgisServer: 'https://maps.carbon.utah.gov',
     quadWord: 'gloria-corona-bombay-prize',
   },
   19: {
     // elections.utah.gov
-    serviceUrl: 'https://mapserv.utah.gov/arcgis/rest/services/LtGovPoliticalDistricts/ExportWebMap/GPServer',
-    exportTaskName: 'Export Web Map',
-    getTemplatesTaskName: '',
+    arcgisServer: 'https://mapserv.utah.gov',
     quadWord: 'andrea-permit-weekend-cable',
   },
   20: {
     // atlas.dev.utah.gov
-    serviceUrl: 'https://mapserv.utah.gov/arcgis/rest/services/Utilities/PrintingTools/GPServer',
-    exportTaskName: 'Export Web Map Task',
-    getTemplatesTaskName: 'Get Layout Templates Info Task',
+    arcgisServer: 'https://mapserv.utah.gov',
     quadWord: 'wedding-tactic-enrico-yes',
   },
 };
