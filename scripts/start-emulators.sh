d1c4f0205c9eb18335e30a542719804c5d304a15
Scott Davis <stdavis@utah.gov>
2026-02-17 13:12:52 -0700

    chore: make sure to use local emulators rather than global

--- scripts/start-emulators.sh
+++ scripts/start-emulators.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # this is in a script file because escaping this stuff in package.json is a pain
-firebase --project=test emulators:start --import .emulator-data --export-on-exit --only firestore "$@" 2> >(grep -Ev 'lsof|Output information may be incomplete|assuming "dev=.*" from mount table' >&2)
+pnpm exec firebase --project=test emulators:start --import .emulator-data --export-on-exit --only firestore "$@" 2> >(grep -Ev 'lsof|Output information may be incomplete|assuming "dev=.*" from mount table' >&2)
